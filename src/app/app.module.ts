0eb42e7bfac5aec35bf3af87ed455320688cddf9
oduorsamuel <soduor15@yahoo.com>
2018-07-11 16:21:07 +0300

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SurveyComponent } from './survey.component';
 import { SurveyEditorComponent } from './survey.editor.component';
 import { QuestionnareComponent } from './questionnare/questionnare.component';
 import { LoginComponent } from './login/login.component';
+import { ResponseService } from './response.service';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,7 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ResponseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/response.service.ts
+++ src/app/response.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class ResponseService {
+    constructor(private http: Http) {}
 
-  constructor() { }
+    storeResponse(response: string) {
+        return this.http.post('http://localhost:8000/', response);
+    }
+    getResponse(y_n) {
+        return this.http.get(`http://localhost:8000/response/${y_n}`);
+    }
 
+    resetResponse() {
+        return this.http.get('http://localhost:8000/reset');
+    }
 }
